1f6686d038b814c1c1e6ce59d2abd5969640fc60
scarf005 <greenscarf005@gmail.com>
2022-04-06 17:03:51 +0900

    test: :white_check_mark: add test for scavtrap

--- ex01/src/main.cpp
+++ ex01/src/main.cpp
@@ -1,33 +1,74 @@
 #include <iostream>
 #include "ScavTrap.hpp"
+#include "test.hpp"
 
 using std::cout;
 
-int main(void) {
+void test_orthodox() {
+  test::header("Construcor and Destructor");
   {
-    std::cout << "===CLAV===\n";
-    ClapTrap trap("trap");
-
-    trap.takeDamage(10);
-    trap.beRepaired(10);
-    trap.attack("some unlucky barrel");
+    test::subject("unnamed");
+    ScavTrap empty;
   }
   {
-    std::cout << "===SCAV EMPTY===\n";
+    test::subject("named");
     ScavTrap scav("scav");
   }
   {
-    std::cout << "===SCAV ACT===\n";
+    test::subject("copy");
     ScavTrap scav("scav");
+    ScavTrap copy(scav);
+  }
+  {
+    test::subject("assign");
+    ScavTrap scav("scav");
+    ScavTrap assign("before assign");
+    assign = scav;
+  }
+}
 
-    scav.attack("some unlucky BARREL");
-    scav.takeDamage(10);
+void test_member_basic() {
+  test::header("attack, takeDamage, beRepaired");
+  ScavTrap scav("scav");
+  test::subject("attack");
+  scav.attack("barrel");
+  test::subject("takeDamage");
+  scav.takeDamage(5);
+  test::subject("beRepaired");
+  scav.beRepaired(3);
+}
+
+void test_member_cant() {
+  {
+    test::header("no hitpoints");
+    ScavTrap scav("scav");
+    scav.takeDamage(300);
+    test::subject("can't attack");
+    scav.attack("barrel");
+    test::subject("can't be repaired");
     scav.beRepaired(10);
-    scav.guardGate();
   }
   {
-    std::cout << "===SCAV CLONE===\n";
+    test::header("no energy");
     ScavTrap scav("scav");
-    ScavTrap scavClone = scav;
+    for (uint i = 0; i < 50; ++i)
+      scav.beRepaired(1);
+    test::subject("can't attack");
+    scav.attack("barrel");
+    test::subject("can't be repaired");
+    scav.beRepaired(10);
   }
+}
+
+void test_guard_gate() {
+  test::header("guard gate");
+  ScavTrap scav("scav");
+  scav.guardGate();
+}
+
+int main(void) {
+  test_orthodox();
+  test_member_basic();
+  test_member_cant();
+  test_guard_gate();
 }
